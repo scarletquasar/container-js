e6b079050ab3f2bdc469a8d4574aa5df2c3a3f92
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-11-28 13:27:11 -0300

    first() development update

--- components/data-enumerators/first.js
+++ components/data-enumerators/first.js
@@ -3,14 +3,29 @@ export const first = (target, type, quantity) => {
     switch(type) {
         case "Array":
             if(quantity === 0) return target[0];
-            break;
+            result = [];
+            while(quantity > 0) {
+                result.push(target[index]);
+                quantity--;
+                index++;
+            }
+            return result;
         
         case "Object":
             const objectEntries = Object.entries(target);
             result = {};
-            if(quantity === 0) {
-                return result[objectEntries[0][0]] = objectEntries[0][1];
+            if(quantity === 0) return result[objectEntries[0][0]] = objectEntries[0][1];
+        
+        case "Set":
+            if(quantity === 0) return new Set(Array.from(target)[0]);
+            result = [];
+            let contentArray = Array.from(target);
+            while(quantity > 0) {
+                result.push(contentArray[index]);
+                quantity--;
+                index++;
             }
+            return result;
     }
 }
 
