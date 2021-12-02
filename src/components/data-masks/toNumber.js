c03b4a18aad6b72183895d98b5eaba2ef536f019
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-02 09:18:47 -0300

    improvement: toNumber()
    Solved performance issues, improved readability with regex.
    Fixed issue #1

--- src/components/data-masks/toNumber.js
+++ src/components/data-masks/toNumber.js
@@ -1,15 +1,7 @@
-//Todo: switch from "map" to "filter" to prevent using character replacement
+
 export const toNumber = (content) => {
     const base = content.toString();
-    const numberComponents = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-    let result = base.split("").map((x) => {
-        if(numberComponents.includes(x)) {
-            return x;
-        }
-        else {
-            return "?";
-        }
-    }).join("").replaceAll("?", "");
+    let result = base.replace(/[^\d.]/g, '');
 
-    return result;
+    return Number(result);
 }
