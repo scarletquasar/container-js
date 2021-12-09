688c499494f011ea1a438c3165ea34bf7ac22b4a
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-08 21:23:48 -0300

    feat: event system wip implementation

--- src/container.js
+++ src/container.js
@@ -12,10 +12,26 @@ class Container {
     #type = null;
     #length = 0;
     #states = [];
+    #events = {
+        change: [],
+        check: [],
+        set: [],
+        get: []
+    };
+
+    #eventBase = {
+        target: this,
+        currentTarget: this,
+        isTrusted: false
+    }
 
     constructor(content) {
         this.#content = content;
         this.#resetAttributes();
+    }
+
+    #triggerEvent(eventName) {
+        this.#events[eventName].forEach(event => event(eventBase));
     }
 
     #resetAttributes() {
@@ -25,30 +41,79 @@ class Container {
     }
 
     //Fetch data from container
-    content = () => this.#content;
-    length = () => this.#length;
-    type = () => this.#type;
-    toString = () => methods.toString(this.content());
-    toNumber = () => methods.toNumber(this.content(), this.type());
-    toBoolean = () => methods.toBoolean(this.content());
-    toSymbol = () => methods.toSymbol(this.content());
-    toBase64 = () => methods.toBase64(this.content());
+    content = () => {
+        this.#triggerEvent("get"); 
+        return this.#content;
+    };
+    length = () => {
+        this.#triggerEvent("check");
+        return this.#length;
+    };
+    type = () => {
+        this.#triggerEvent("check");
+        return this.#type;
+    };
+    toString = () => {
+        this.#triggerEvent("get");
+        return methods.toString(this.content());
+    };
+    toNumber = () => {
+        this.#triggerEvent("get");
+        return methods.toNumber(this.content(), this.type())
+    };
+    toBoolean = () => {
+        this.#triggerEvent("get");
+        return methods.toBoolean(this.content());
+    };
+    toSymbol = () => {
+        this.#triggerEvent("get");
+        return methods.toSymbol(this.content());
+    };
+    toBase64 = () => {
+        this.#triggerEvent("get");
+        return methods.toBase64(this.content());
+    };
 
     //Fetch changed data from container in a new container
-    first = (quantity) => Container.from(methods.first(this.content(), this.type(), quantity));
-    last = (quantity) => Container.from(methods.last(this.content(), this.type(), quantity, this.length()));
-    skip = (quantity) => Container.from(methods.skip(this.content(), this.type(), quantity));
-    where = (condition) => Container.from(methods.where(condition, this.content(), this.type()));
+    first = (quantity) => {
+        this.#triggerEvent("get");
+        return Container.from(methods.first(this.content(), this.type(), quantity));
+    };
+    last = (quantity) => {
+        this.#triggerEvent("get");
+        return Container.from(methods.last(this.content(), this.type(), quantity, this.length()))
+    };
+    skip = (quantity) => {
+        this.#triggerEvent("get");
+        return Container.from(methods.skip(this.content(), this.type(), quantity));
+    };
+    where = (condition) => {
+        this.#triggerEvent("get");
+        return Container.from(methods.where(condition, this.content(), this.type()));
+    };
 
     //Container operations
-    forEach = (...args) => methods.forEach(...args, this.content(), this.type());
-    map = (...args) => methods.map(...args, this.content(), this.type());
-    strictForEach = (callback, condition) => methods.strictForEach(callback, condition, this.content(), this.type());
-    strictMap = (callback, condition) => methods.strictMap(callback, condition, this.content(), this.type());
+    forEach = (...args) => {
+        this.#triggerEvent("get");
+        methods.forEach(...args, this.content(), this.type());
+    };
+    map = (...args) => {
+        this.#triggerEvent("get");
+        methods.map(...args, this.content(), this.type());
+    };
+    strictForEach = (callback, condition) => {
+        this.#triggerEvent("get");
+        methods.strictForEach(callback, condition, this.content(), this.type());
+    };
+    strictMap = (callback, condition) => {
+        this.#triggerEvent("get");
+        methods.strictMap(callback, condition, this.content(), this.type());
+    };
 
     //Edit data in the container
     set = (newContent) => {
         if(methods.set(newContent, this.type())) {
+            this.#triggerEvent("set");
             this.#content = newContent;
             this.#resetAttributes();
         }
@@ -56,7 +121,10 @@ class Container {
             throw new TypeError(this.#errors.notAssignable);
         }
     }
-    saveState = () => this.#states.push(this.content());
+    saveState = () => {
+        this.#triggerEvent("set");
+        this.#states.push(this.content());
+    };
     getState = (index) => this.#states[index];
     restoreState = (index) => this.set(this.getState(index));
     add = (...args) => Container.from(methods.add(this.type(), this, ...args));
